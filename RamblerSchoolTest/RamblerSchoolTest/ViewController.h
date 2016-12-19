7817836152a0a64b31a508c33f87ef7c6e960e6a
Vasyura Anastasiya <a.vasyura@rambler-co.ru>
2016-12-19 11:37:02 +0300

    Добавлен проект

---
+++ RamblerSchoolTest/RamblerSchoolTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RamblerSchoolTest
+//
+//  Created by Vasyura Anastasiya on 19/12/2016.
+//  Copyright © 2016 Rambler&Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
